96aa8cb59ba3fd05b23bcd07a7df6f2a365f1ada
Ray198003 <89965511+Ray198003@users.noreply.github.com>
2021-09-02 12:47:44 +0200

    modificada estructura

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){ //s'esoera a que el document html estigui carregat
-  var persones =["joan", "maria", "pepe", "miquel", "oriol", "julia", "anna"]; //arrray
+  var persones =["julia", "maria", "pepe", "miquel", "oriol", "julia", "anna"]; //arrray
   for(n=0; n<persones.length; n++){ //recorrem l'array
     $("div").append("<p>"+persones[n]+"</p>"); //escrivim en el div la persona del array
   }
